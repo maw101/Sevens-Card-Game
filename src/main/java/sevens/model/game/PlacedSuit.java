0c9b726ffb8a58b7219ebfa4c09a7e0a4dd5f447
maw101 <maw101@aber.ac.uk>
2020-06-10 16:23:03 +0100

    Added PlacedSuit constructor and setters/getters

--- src/main/java/sevens/model/game/PlacedSuit.java
+++ src/main/java/sevens/model/game/PlacedSuit.java
@@ -8,6 +8,34 @@ public class PlacedSuit {
     private Card highestCard;
     private int suit;
 
-    
+    public PlacedSuit() {
+        this.lowestCard = null;
+        this.highestCard = null;
+        this.suit = 0;
+    }
+
+    public Card getLowestCard() {
+        return this.lowestCard;
+    }
+
+    public Card getHighestCard() {
+        return this.highestCard;
+    }
+
+    public int getSuit() {
+        return this.suit;
+    }
+
+    public void setLowestCard(Card lowestCard) {
+        this.lowestCard = lowestCard;
+    }
+
+    public void setHighestCard(Card highestCard) {
+        this.highestCard = highestCard;
+    }
+
+    public void setSuit(int suit) {
+        this.suit = suit;
+    }
 
 }

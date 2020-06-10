b788d617ed0bafac66721efc07fd2391b8c77a36
maw101 <maw101@aber.ac.uk>
2020-06-10 16:35:19 +0100

    Created base class for main model

---
+++ src/main/java/sevens/model/game/SevensGame.java
@@ -0,0 +1,21 @@
+package sevens.model.game;
+
+import sevens.model.carddeck.Deck;
+import sevens.model.carddeck.Hand;
+
+public class SevensGame {
+
+    private int numberOfPlayers;
+    private Deck deck;
+    private Hand[] hands;
+    private PlacedSuit[] playedCards; // Index 0=DIAMONDS; 1=CLUBS; 2=HEARTS; 3=SPADES
+
+    // Card Constants:
+    //  DIAMOND = 1
+    //  CLUB = 2
+    //  HEART = 3
+    //  SPADE = 4
+
+    
+
+}

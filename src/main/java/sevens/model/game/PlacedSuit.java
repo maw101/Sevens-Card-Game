19f4a378c5e47c2c2c6d7af7f0d58de8291f4ae3
maw101 <maw101@aber.ac.uk>
2020-06-10 16:21:22 +0100

    Added PlacedSuit attributes

--- src/main/java/sevens/model/game/PlacedSuit.java
+++ src/main/java/sevens/model/game/PlacedSuit.java
@@ -1,6 +1,12 @@
 package sevens.model.game;
 
+import sevens.model.carddeck.Card;
+
 public class PlacedSuit {
+
+    private Card lowestCard;
+    private Card highestCard;
+    private int suit;
 
     
 

c046a945777d5e818b59da45cb61dbc13becdf88
maw101 <maw101@aber.ac.uk>
2020-06-11 14:35:40 +0100

    Tidy: Added all JavaDoc and tidied SevensGame

--- src/main/java/sevens/model/game/SevensGame.java
+++ src/main/java/sevens/model/game/SevensGame.java
@@ -1,4 +1,6 @@
-// TODO: add file header
+/*
+    SevensGame.java 1.0 2020/06/11
+ */
 package sevens.model.game;
 
 import sevens.model.carddeck.Card;
@@ -7,8 +9,20 @@ import sevens.model.carddeck.Hand;
 
 import java.util.ArrayList;
 
-// TODO: add class JavaDoc
+/**
+ * Represents a sevens card game.
+ * <p>
+ * Stores attributes related to the game such as the played cards and players
+ * hands. Provides methods to interfaces to facilitate the playing of the game.
+ *
+ * @author Matthew Williams
+ * @version 1.0     Initial Implementation
+ */
 public class SevensGame {
+
+    // //////////////// //
+    // Class variables. //
+    // //////////////// //
 
     private final int numberOfPlayers;
     private Hand[] hands;
@@ -21,73 +35,79 @@ public class SevensGame {
     //  HEART = 3
     //  SPADE = 4
 
-    // TODO: add method JavaDoc
+    // ///////////// //
+    // Constructors. //
+    // ///////////// //
+
+    /**
+     * Instantiates a new Sevens game.
+     * <p>
+     * Sets up the game with the default number of players (4)
+     */
     public SevensGame() {
-        // setup game with default number of players (4)
         this(4);
     }
 
-    // TODO: add method JavaDoc
+    /**
+     * Instantiates a new Sevens game.
+     *
+     * @param numberOfPlayers the number of players
+     */
     public SevensGame(int numberOfPlayers) {
         this.numberOfPlayers = numberOfPlayers;
         this.currentPlayerNumber = 1;
         setupNewGame();
     }
 
-    // TODO: add method JavaDoc
-    public boolean isValidMove(Card cardToPlay) {
-        // get cards suit
-        int cardsSuit = cardToPlay.getSuit();
-        // get index of current suit
-        int suitIndex = cardsSuit - 1;
+    // ///////////////////// //
+    // Read-only properties. //
+    // ///////////////////// //
 
-        // lookup the suit in the playedCards array
-        PlacedSuit placedSuit = playedCards[suitIndex];
-
-        return placedSuit.canCardBePlaced(cardToPlay);
+    /**
+     * Gets the number of players.
+     *
+     * @return the number of players
+     */
+    public int getNumberOfPlayers() {
+        return numberOfPlayers;
     }
 
-    // TODO: add method JavaDoc
-    public ArrayList<Card> getAllValidMoves(int playerNumber) {
-        // create a new list to keep track of valid moves
-        ArrayList<Card> validMoves = new ArrayList<>();
-
-        // get player index and determine if the player number arg is valid
-        int playerIndex = playerNumber - 1;
-        checkIsValidPlayerIndex(playerIndex);
-        // get the players hand
-        Hand playersHand = hands[playerIndex];
-
-        // iterate over each card in the players hand
-        for (Card move : playersHand.getHand()) {
-            // determine if can be played, if so add to our list of valid moves
-            if (isValidMove(move)) {
-                validMoves.add(move);
-            }
-        }
-
-        return validMoves;
+    /**
+     * Get all players hands
+     *
+     * @return an array containing all players hands
+     */
+    public Hand[] getHands() {
+        return hands;
     }
 
-    // TODO: add method JavaDoc
-    private void checkIsValidPlayerIndex(int playerIndex)
-        throws IllegalArgumentException {
-        if ((playerIndex < 0) || (playerIndex >= numberOfPlayers)) {
-            throw new IllegalArgumentException("Invalid Player Number");
-        }
+    /**
+     * Get each of the four placed card suits.
+     *
+     * @return an array containing the four placed card suits.
+     */
+    public PlacedSuit[] getPlayedCards() {
+        return playedCards;
     }
 
-    // TODO: add method JavaDoc
-    public boolean hasPlayerWon(int playerNumber) {
-        // get player index and determine if the player number arg is valid
-        int playerIndex = playerNumber - 1;
-        checkIsValidPlayerIndex(playerIndex);
-
-        // determine if the player has no cards left to play
-        return hands[playerIndex].getCardCount() == 0;
+    /**
+     * Gets the current player number.
+     *
+     * @return the current player number
+     */
+    public int getCurrentPlayerNumber() {
+        return currentPlayerNumber;
     }
 
-    // TODO: add method JavaDoc
+    // //////// //
+    // Methods. //
+    // //////// //
+
+    /**
+     * Sets up a new game.
+     * <p>
+     * Resets each of the four placed suits and shuffles and deals the cards.
+     */
     public void setupNewGame() {
         playedCards = new PlacedSuit[4];
 
@@ -100,7 +120,12 @@ public class SevensGame {
         performInitialDeal();
     }
 
-    // TODO: add method JavaDoc
+    /**
+     * Performs an initial deal to all players.
+     * <p>
+     * Shuffles the card deck then deals all cards starting from the first
+     * player and cycling around until all cards are dealt.
+     */
     private void performInitialDeal() {
         Card dealtCard;
         int currentPlayerIndex = 0;
@@ -127,39 +152,115 @@ public class SevensGame {
         }
     }
 
-    // TODO: add method JavaDoc
-    public int getNumberOfPlayers() {
-        return numberOfPlayers;
-    }
-
-    // TODO: add method JavaDoc
-    public Hand[] getHands() {
-        return hands;
-    }
-
-    // TODO: add method JavaDoc
-    public PlacedSuit[] getPlayedCards() {
-        return playedCards;
-    }
-
-    // TODO: add method JavaDoc
-    public int getCurrentPlayerNumber() {
-        return currentPlayerNumber;
-    }
-
-    // TODO: add method JavaDoc
+    /**
+     * Moves onto the next player.
+     */
     public void nextPlayer() {
         this.currentPlayerNumber++;
+        // TODO: need to adapt to cycle back around when on the last player
     }
 
-    // TODO: add method JavaDoc
-    public Hand getPlayersHand(int playerNumber) {
+    /**
+     * Determines if a given card can be played.
+     *
+     * @param cardToPlay the card to play
+     * @return  true if the card can be played;
+     *          false otherwise.
+     */
+    public boolean isValidMove(Card cardToPlay) {
+        // get cards suit
+        int cardsSuit = cardToPlay.getSuit();
+        // get index of current suit
+        int suitIndex = cardsSuit - 1;
+
+        // lookup the suit in the playedCards array
+        PlacedSuit placedSuit = playedCards[suitIndex];
+
+        return placedSuit.canCardBePlaced(cardToPlay);
+    }
+
+    /**
+     * Gets all valid moves for a given players hand.
+     *
+     * @param playerNumber the player number
+     * @return the all valid moves the given player can make
+     */
+    public ArrayList<Card> getAllValidMoves(int playerNumber) {
+        // create a new list to keep track of valid moves
+        ArrayList<Card> validMoves = new ArrayList<>();
+
+        // get player index and determine if the player number arg is valid
         int playerIndex = playerNumber - 1;
-        checkIsValidPlayerIndex(playerIndex); // determine if the player number is valid
+        checkIsValidPlayerIndex(playerIndex);
+        // get the players hand
+        Hand playersHand = hands[playerIndex];
+
+        // iterate over each card in the players hand
+        for (Card move : playersHand.getHand()) {
+            // determine if can be played, if so add to our list of valid moves
+            if (isValidMove(move)) {
+                validMoves.add(move);
+            }
+        }
+
+        return validMoves;
+    }
+
+    /**
+     * Determines if a player index is within the acceptable range or not.
+     *
+     * @param playerIndex the player index to test
+     * @throws IllegalArgumentException if the argument is an invalid player
+     *                                  number - something is wrong with the
+     *                                  implemented interface
+     */
+    private void checkIsValidPlayerIndex(int playerIndex)
+        throws IllegalArgumentException {
+        if ((playerIndex < 0) || (playerIndex >= numberOfPlayers)) {
+            throw new IllegalArgumentException("Invalid Player Number");
+        }
+    }
+
+    /**
+     * Determines if a given player has won.
+     *
+     * @param playerNumber the player number to check
+     * @return  true if the given player has no cards left (ie has won);
+     *          false otherwise.
+     */
+    public boolean hasPlayerWon(int playerNumber) {
+        // get player index and determine if the player number arg is valid
+        int playerIndex = playerNumber - 1;
+        checkIsValidPlayerIndex(playerIndex);
+
+        // determine if the player has no cards left to play
+        return hands[playerIndex].getCardCount() == 0;
+    }
+
+    /**
+     * Gets a given players hand.
+     *
+     * @param playerNumber the player number
+     * @return the players hand
+     */
+    public Hand getPlayersHand(int playerNumber) {
+        // get player index and determine if the player number arg is valid
+        int playerIndex = playerNumber - 1;
+        checkIsValidPlayerIndex(playerIndex);
+
         return hands[playerIndex];
     }
 
-    // TODO: add method JavaDoc
+    /**
+     * Makes a given move.
+     * <p>
+     * Removes the given card from the players hand then places this on the
+     * appropriate placed suit.
+     * Does not perform checking, this must be done using the other methods
+     * provided.
+     *
+     * @param cardToPlay the card to play
+     */
     public void makeMove(Card cardToPlay) {
         // remove card from players hand
         removeCardFromPlayersHand(currentPlayerNumber, cardToPlay);
@@ -167,7 +268,14 @@ public class SevensGame {
         placeCard(cardToPlay);
     }
 
-    // TODO: add method JavaDoc
+    /**
+     * Places a given card in the appropriate placed suit.
+     * <p>
+     * Does not perform checking, this must be done using the other methods
+     * provided.
+     *
+     * @param cardToPlay the card to play
+     */
     private void placeCard(Card cardToPlay) {
         // assumes is valid move
 
@@ -186,18 +294,24 @@ public class SevensGame {
             placedSuit.setLowestCard(cardToPlay);
             placedSuit.setHighestCard(cardToPlay);
 
-        // if card to play is greater than a seven update highest
+            // if card to play is greater than a seven update highest
         } else if (cardsRank > 7) {
             placedSuit.setHighestCard(cardToPlay);
 
-        // if card to play is less than than a seven update lowest
+            // if card to play is less than than a seven update lowest
         } else { // cardsRank < 7
             placedSuit.setLowestCard(cardToPlay);
         }
     }
 
-    // TODO: add method JavaDoc
+    /**
+     * Removes a given card from the players hand.
+     *
+     * @param playerNumber the number of the player whose hand holds the card
+     * @param cardToRemove the card to remove
+     */
     private void removeCardFromPlayersHand(int playerNumber, Card cardToRemove) {
+        // get player index and determine if the player number arg is valid
         int playerIndex = playerNumber - 1;
         checkIsValidPlayerIndex(playerIndex);
 

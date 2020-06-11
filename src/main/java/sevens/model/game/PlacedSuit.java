9d01539bd3e084c00620b794ccf7450d8c0bf485
maw101 <maw101@aber.ac.uk>
2020-06-11 14:21:07 +0100

    Tidy: Added all JavaDoc and tidied PlacedSuit

--- src/main/java/sevens/model/game/PlacedSuit.java
+++ src/main/java/sevens/model/game/PlacedSuit.java
@@ -1,60 +1,122 @@
-// TODO: add file header
+/*
+    PlacedSuit.java 1.0 2020/06/11
+ */
 package sevens.model.game;
 
 import sevens.model.carddeck.Card;
 
-// TODO: add class JavaDoc
+/**
+ * Represents a placed suit.
+ * <p>
+ * Stores attributes related to a suit that has been placed down - the highest
+ * card, the lowest card and the suits identifier.
+ *
+ * @author Matthew Williams
+ * @version 1.0     Initial Implementation
+ */
 public class PlacedSuit {
+
+    // //////////////// //
+    // Class variables. //
+    // //////////////// //
 
     private Card lowestCard;
     private Card highestCard;
     private int suit;
 
-    // TODO: add method JavaDoc
+    // ///////////// //
+    // Constructors. //
+    // ///////////// //
+
+    /**
+     * Instantiates a new Placed suit.
+     */
     public PlacedSuit() {
         this.lowestCard = null;
         this.highestCard = null;
         this.suit = 0;
     }
 
-    // TODO: add method JavaDoc
+    // ////////////////////// //
+    // Read/Write properties. //
+    // ////////////////////// //
+
+    /**
+     * Gets lowest card.
+     *
+     * @return the lowest card
+     */
     public Card getLowestCard() {
         return this.lowestCard;
     }
 
-    // TODO: add method JavaDoc
+    /**
+     * Sets lowest card.
+     *
+     * @param lowestCard the new lowest card
+     */
+    public void setLowestCard(Card lowestCard) {
+        this.lowestCard = lowestCard;
+    }
+
+    /**
+     * Gets highest card.
+     *
+     * @return the highest card
+     */
     public Card getHighestCard() {
         return this.highestCard;
     }
 
-    // TODO: add method JavaDoc
+    /**
+     * Sets highest card.
+     *
+     * @param highestCard the new highest card
+     */
+    public void setHighestCard(Card highestCard) {
+        this.highestCard = highestCard;
+    }
+
+    /**
+     * Gets suit.
+     *
+     * @return the suit
+     */
     public int getSuit() {
         return this.suit;
     }
 
-    // TODO: add method JavaDoc
+    /**
+     * Sets suit.
+     *
+     * @param suit the new suit
+     */
+    public void setSuit(int suit) {
+        this.suit = suit;
+    }
+
+    // //////// //
+    // Methods. //
+    // //////// //
+
+    /**
+     * Gets suit name.
+     *
+     * @return the suit name
+     */
     public String getSuitName() {
         // create a dummy card
         Card dummyCard = new Card(this.suit, 2); // random rank
         return dummyCard.getSuitString();
     }
 
-    // TODO: add method JavaDoc
-    public void setLowestCard(Card lowestCard) {
-        this.lowestCard = lowestCard;
-    }
-
-    // TODO: add method JavaDoc
-    public void setHighestCard(Card highestCard) {
-        this.highestCard = highestCard;
-    }
-
-    // TODO: add method JavaDoc
-    public void setSuit(int suit) {
-        this.suit = suit;
-    }
-
-    // TODO: add method JavaDoc
+    /**
+     * Determines if a card can be placed down.
+     *
+     * @param cardToPlay the card to play
+     * @return  true if the card can be played on the current placed suit;
+     *          false otherwise.
+     */
     public boolean canCardBePlaced(Card cardToPlay) {
         // get cards rank
         int cardsRank = cardToPlay.getRank();
@@ -76,7 +138,11 @@ public class PlacedSuit {
         }
     }
 
-    // TODO: add method JavaDoc
+    /**
+     * Returns a string representation of the placed suit.
+     *
+     * @return a string representation of the placed suit
+     */
     @Override
     public String toString() {
         String suitName = getSuitName();

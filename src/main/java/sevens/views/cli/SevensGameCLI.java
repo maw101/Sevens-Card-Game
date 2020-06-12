117f8a017b873b1035941c6af86ef473bcf4cc0f
maw101 <maw101@aber.ac.uk>
2020-06-12 15:14:15 +0100

    Added basic constructor and basic input methods

--- src/main/java/sevens/views/cli/SevensGameCLI.java
+++ src/main/java/sevens/views/cli/SevensGameCLI.java
@@ -2,10 +2,47 @@ package sevens.views.cli;
 
 import sevens.model.game.SevensGame;
 
+import java.util.Scanner;
+
 public class SevensGameCLI {
 
-    private SevensGame model;
-    private int numberOfComputerPlayers;
+    private final SevensGame model;
+    private final int numberOfComputerPlayers;
+    private final static Scanner in = new Scanner(System.in);
+
+    public SevensGameCLI() {
+        int totalPlayers = getNumberOfPlayersAsInput();
+        // setup new game with that number of players
+        model = new SevensGame(totalPlayers);
+        numberOfComputerPlayers = getNumberOfComputerPlayersAsInput();
+    }
+
+    private int getNumberOfPlayersAsInput() {
+        System.out.println("Enter the Number of Players: ");
+        int numberOfPlayers;
+        do {
+            numberOfPlayers = in.nextInt();
+            if ((numberOfPlayers < 2) || (numberOfPlayers > 52)) {
+                System.err.println("Number of players must be greater than 1 and less than 53");
+                System.out.println("Try again: ");
+            }
+        } while ((numberOfPlayers < 2) || (numberOfPlayers > 52));
+        return numberOfPlayers;
+    }
+
+    private int getNumberOfComputerPlayersAsInput() {
+        int totalPlayers = this.model.getNumberOfPlayers();
+        System.out.println("Enter the Number of Players you wish to be Computer Players: ");
+        int numberOfComputerPlayers;
+        do {
+            numberOfComputerPlayers = in.nextInt();
+            if ((numberOfComputerPlayers < 2) || (numberOfComputerPlayers > 52)) {
+                System.err.println("Number of computer players must be 0 or more, but less than or equal to the total number of players.");
+                System.out.println("Try again: ");
+            }
+        } while ((numberOfComputerPlayers < 0) || (numberOfComputerPlayers > totalPlayers));
+        return numberOfComputerPlayers;
+    }
 
     
 

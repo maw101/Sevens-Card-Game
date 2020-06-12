95f868c4ad7547dfd86544d674edbd40e82cbc33
maw101 <maw101@aber.ac.uk>
2020-06-12 15:05:57 +0100

    Added initial CLI class implementation

---
+++ src/main/java/sevens/views/cli/SevensGameCLI.java
@@ -0,0 +1,12 @@
+package sevens.views.cli;
+
+import sevens.model.game.SevensGame;
+
+public class SevensGameCLI {
+
+    private SevensGame model;
+    private int numberOfComputerPlayers;
+
+    
+
+}

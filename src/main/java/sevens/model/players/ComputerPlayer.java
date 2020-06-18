079a8e8f6975496f35f1a56d64f274a058f3cb52
maw101 <maw101@aber.ac.uk>
2020-06-18 17:10:06 +0100

    Added base Computer Player Class

---
+++ src/main/java/sevens/model/players/ComputerPlayer.java
@@ -0,0 +1,21 @@
+package sevens.model.players;
+
+import sevens.model.carddeck.Card;
+import sevens.model.carddeck.Hand;
+import sevens.model.game.PlacedSuit;
+import sevens.model.game.SevensGame;
+
+import java.util.*;
+
+public class ComputerPlayer
+        implements Player {
+
+    public Card getMove(SevensGame model, int currentPlayerNumber) {
+        return null;
+    }
+
+    private int scoreCard(SevensGame model, Hand playersHand, Card assessedCard) {
+        return 0;
+    }
+
+}
